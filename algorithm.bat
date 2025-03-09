b69f7a72324b29234e08456a575cae0b6364d6b8
Faris Saib Fadhil <157056193+FarisSFadhil@users.noreply.github.com>
2025-03-09 18:07:09 -0400

    Create algorithm.bat

---
+++ algorithm.bat
@@ -0,0 +1,6 @@
+@echo off
+echo MODIFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION OF PI > report.txt
+echo 3.14159 >> report.txt
+
+rem Uncomment the following line and remove the previous echo statements to integrate a precise calculation tool.
+rem echo scale=1000; 4*a(1) | bc -l > report.txt
